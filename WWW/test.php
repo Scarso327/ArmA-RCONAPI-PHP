c9d61225cfa6fcc5e27a4c2f14de2afb6835d5e8
Jack Farhall <jackfarhall@gmail.com>
2018-12-25 18:50:27 +0000

    Added: My test file (Shows how commands can be used)

---
+++ WWW/test.php
@@ -0,0 +1,6 @@
+<?php
+// This file contains the functions I've been testing...
+
+include_once '../_API/includes.php';
+//commands::sendMessage(true, -1, "test"); // Sends a global message of "test" if -1 was 0 and above it would send it to a player with that id
+//commands::banPlayer(true, 0, 1, "test"); // Bans the player with id 1 for 1 minute with reason "test"
